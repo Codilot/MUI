74e3c9bc6fff47364db481ab841ac5e206340d3e
Codilot <e.kooijman@gmail.com>
2018-08-31 12:44:57 +0200

    Adds data store

---
+++ src/store.js
@@ -0,0 +1,76 @@
+export const plantFamilies = [
+    'Brassicaceae', 'Amaranthaceae', 'Asteraceae', 'Apiaceae', 'Amaryllidaceae', 'Leguminosae'
+]
+
+export const plants = [
+    {
+        id: 'leek',
+        name: 'Leek',
+        soil: 'Middle fertile soil',
+        position: 'sunny',
+        family: 'Amaryllidaceae'
+    },
+    {
+        id: 'garlic',
+        name: 'Garlic',
+        soil: 'Middle fertile soil',
+        position: 'sunny',
+        family: 'Amaryllidaceae'
+    },
+    {
+        id: 'broccoli',
+        name: 'Broccoli',
+        soil: 'Rich fertile soil',
+        position: 'sunny',
+        family: 'Brassicaceae'
+    },
+    {
+        id: 'curly-kale',
+        name: 'Curly kale',
+        soil: 'Rich fertile soil',
+        position: 'sunny',
+        family: 'Brassicaceae'
+    },
+    {
+        id: 'crisphead-lettuce',
+        name: 'Crisphead lettuce',
+        soil: 'Middle fertile soil',
+        position: 'partial shadow',
+        family: 'Asteraceae'
+    },
+    {
+        id: 'chamomile',
+        name: 'Chamomile',
+        soil: 'Low fertile soil',
+        position: 'sunny',
+        family: 'Asteraceae'
+    },
+    {
+        id: 'red-beet',
+        name: 'Red beet',
+        soil: 'Middle fertile soil',
+        position: 'partial shadow',
+        family: 'Amaranthaceae'
+    },
+    {
+        id: 'card',
+        name: 'card',
+        soil: 'Middle fertile soil',
+        position: 'partial shadow',
+        family: 'Amaranthaceae'
+    },
+    {
+        id: 'sugar-snap',
+        name: 'Sugar snap',
+        soil: 'Low fertile soil',
+        position: 'sunny',
+        family: 'Leguminosae'
+    },
+    {
+        id: 'sweet-pea',
+        name: 'Sweet pea',
+        soil: 'Low fertile soil',
+        position: 'sunny',
+        family: 'Leguminosae'
+    }
+]
